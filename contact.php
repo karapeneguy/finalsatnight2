e736b32660d1c492161d0612d423088d7ba22a9f
karapeneguy <helpdesk@helps-imac-206.smc.loyno.edu>
2018-05-06 14:06:16 -0500

    added stuff to contact page

--- contact.php
+++ contact.php
@@ -1,2 +1,33 @@
 <?php include "templates/header.php"; ?>
+<br>
+<br>
+<br>
+<br>
+
+
+
+<h2>Booking</h2>
+<p>booking@karawolfe.com</p>
+
+<h2>Management</h2>
+<p>management@karawolfe.com</p>
+
+<h2>Publishing</h2>
+<p>publishing@karawolfe.com</p>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
 <?php include "templates/footer.php"; ?>
